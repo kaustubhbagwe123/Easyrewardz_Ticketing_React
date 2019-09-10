8719afa598164298f971d03ec268fd4ced23dd25
Chetan Kokal <chetan.kokal@brainvire.com>
2019-09-10 14:17:34 +0530

    aux.js replaced with child.js

--- src/Component/layout.js
+++ src/Component/layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Header from "./Header";
-import Aux from "./Aux";
+import Aux from "./child";
 
 export default class layout extends Component {
   render() {
