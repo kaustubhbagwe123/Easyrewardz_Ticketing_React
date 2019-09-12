136f91173485c703d61ea29002e63cccb5f1c95e
Deepak Savani <deepak.savani@brainvire.com>
2019-09-12 15:26:04 +0530

    claim tab ticket view page add

--- src/routes/ClaimTabTicketView.js
+++ src/routes/ClaimTabTicketView.js
@@ -4,7 +4,7 @@ import Modal from "react-modal";
 
 class ClaimTabTicketView extends Component {
   constructor(props) {
-    Modal.setAppElement("body");
+    Modal.setAppElement("");
     super(props);
 
     this.state = {
