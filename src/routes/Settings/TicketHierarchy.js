7044de2540dc0d117c1ad25477793dc5aeea8775
Shlok Barot <shlok.barot@brainvire.com>
2019-09-13 15:32:17 +0530

    Add file

---
+++ src/routes/Settings/TicketHierarchy.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class TicketHierarchy extends Component {
+    render() {
+        return (
+            <div>
+                <div className="hiearchyRow">
+                hii
+                </div>
+                
+            </div>
+        )
+    }
+}
+
+export default TicketHierarchy;
