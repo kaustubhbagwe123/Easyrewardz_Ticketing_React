cbf7c02227ec85c7b40b28f124c4981e64e48346
Shlok Barot <shlok.barot@brainvire.com>
2019-09-24 10:50:17 +0530

    made File Upload logs page

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,6 +15,7 @@ import QADashboardSearch from "./QA/QADashboardSearch";
 import ChatDashboard from "./Chat/ChatDashboard";
 import QAAuditview from "./QA/QAAuditview";
 import KnowledgeBase from "./KnowledgeBase";
+import FileUploadLogs from "./Settings/FileUploadLogs";
 // import Aux from "./../Component/Aux";
 
 export class App extends Component {
@@ -73,6 +74,10 @@ export class App extends Component {
           path={`${match.url}/qaauditview`}
           component={QAAuditview}
         />
+        <Route 
+        exact
+        path={`${match.url}/fileUploadLogs`}
+        component={FileUploadLogs}/>
       </Layout>
     );
   }
