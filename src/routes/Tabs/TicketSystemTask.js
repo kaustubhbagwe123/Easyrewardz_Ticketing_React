1b74eca4876d38736676886a65144d101a7480f5
Shlok Barot <shlok.barot@brainvire.com>
2019-12-17 11:08:46 +0530

    error solve

--- src/routes/Tabs/TicketSystemTask.js
+++ src/routes/Tabs/TicketSystemTask.js
@@ -49,124 +49,129 @@ class TicketSystemTask extends Component {
     return (
       <Fragment>
         <div className="ticketSycard">
-      <div className="ticketSycard1">
-        <div id="accordion">
-          <div className="">
-            <div className="card-header collapsetask" id="headingOne">
-              <h5 className="mb-8 drop">
-                <label
-                  className="btn btn-link drop1"
-                  data-toggle="collapse"
-                  data-target="#collapseOne"
-                  aria-expanded="true"
-                  aria-controls="collapseOne"
-                >
-                  Created Task
-                </label>
-              </h5>
-            </div>
+          <div className="ticketSycard1">
+            <div id="accordion">
+              <div className="">
+                <div className="card-header collapsetask" id="headingOne">
+                  <h5 className="mb-8 drop">
+                    <label
+                      className="btn btn-link drop1"
+                      data-toggle="collapse"
+                      data-target="#collapseOne"
+                      aria-expanded="true"
+                      aria-controls="collapseOne"
+                    >
+                      Created Task
+                    </label>
+                  </h5>
+                </div>
 
-            <div
-              id="collapseOne"
-              className="collapse show"
-              aria-labelledby="headingOne"
-              data-parent="#accordion"
-            >
-              <div className="card-body">
-                <div className="row m-b-10">
-                  <div className="col-md-12">
-                    <select className="category-select-system">
+                <div
+                  id="collapseOne"
+                  className="collapse show"
+                  aria-labelledby="headingOne"
+                  data-parent="#accordion"
+                >
+                  <div className="card-body">
+                    <div className="row m-b-10">
+                      <div className="col-md-12">
+                        {/* <select className="category-select-system">
                       <option className="select-category-placeholder">
                         Department
                       </option>
-                    </select>
-                  </div>
-                </div>
-                <div className="row m-b-10">
-                  <div className="col-md-12">
-                    <textarea
-                      className="addNote-textarea-system"
-                      placeholder="Task Description"
-                    ></textarea>
-                  </div>
-                </div>
-                <div className="row m-b-10">
-                  <div className="col-md-6">
-                    <select className="category-select-system dropdown-label">
-                      <option className="select-category-placeholder dropdown-label">
-                        Department
-                      </option>
-                    </select>
-                  </div>
-                  <div className="col-md-6">
-                    <select className="category-select-system dropdown-label">
-                      <option className="select-sub-category-placeholder">
-                        Function
-                      </option>
-                    </select>
-                  </div>
-                </div>
-                <div className="row m-b-10">
-                  <div className="col-md-6">
-                    <select className="category-select-system dropdown-label">
-                      <option className="select-category-placeholder dropdown-label">
-                        Assign To
-                      </option>
-                    </select>
-                  </div>
-                  <div className="col-md-6">
-                    <select className="category-select-system dropdown-label">
-                      <option className="select-sub-category-placeholder">
-                        Priority
-                      </option>
-                    </select>
-                  </div>
-                </div>
-                <div className="row m-b-10">
-                  <div className="col-md-6">
-                    <button type="button" className="createtasksystem">
-                      <label className="createtasksystem-text">
-                        CREATE TASK
-                      </label>
-                    </button>
+                    </select> */}
+                        <input
+                          type="text"
+                          className="txt-1"
+                          placeholder="Task Title"
+                        />
+                      </div>
+                    </div>
+                    <div className="row m-b-10">
+                      <div className="col-md-12">
+                        <textarea
+                          className="addNote-textarea-system"
+                          placeholder="Task Description"
+                        ></textarea>
+                      </div>
+                    </div>
+                    <div className="row m-b-10">
+                      <div className="col-md-6">
+                        <select className="category-select-system dropdown-label">
+                          <option className="select-category-placeholder dropdown-label">
+                            Department
+                          </option>
+                        </select>
+                      </div>
+                      <div className="col-md-6">
+                        <select className="category-select-system dropdown-label">
+                          <option className="select-sub-category-placeholder">
+                            Function
+                          </option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="row m-b-10">
+                      <div className="col-md-6">
+                        <select className="category-select-system dropdown-label">
+                          <option className="select-category-placeholder dropdown-label">
+                            Assign To
+                          </option>
+                        </select>
+                      </div>
+                      <div className="col-md-6">
+                        <select className="category-select-system dropdown-label">
+                          <option className="select-sub-category-placeholder">
+                            Priority
+                          </option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="row m-b-10">
+                      <div className="col-md-6">
+                        <button type="button" className="createtasksystem">
+                          <label className="createtasksystem-text">
+                            CREATE TASK
+                          </label>
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="">
-            <div className="card-header collapsetask" id="headingTwo">
-              <h5 className="mb-0 drop">
-                <label
-                  className="btn btn-link collapsed drop1"
-                  data-toggle="collapse"
-                  data-target="#collapseTwo"
-                  aria-expanded="false"
-                  aria-controls="collapseTwo"
+              <div className="">
+                <div className="card-header collapsetask" id="headingTwo">
+                  <h5 className="mb-0 drop">
+                    <label
+                      className="btn btn-link collapsed drop1"
+                      data-toggle="collapse"
+                      data-target="#collapseTwo"
+                      aria-expanded="false"
+                      aria-controls="collapseTwo"
+                    >
+                      05 Task Created
+                    </label>
+                  </h5>
+                </div>
+                <div
+                  id="collapseTwo"
+                  className="collapse"
+                  aria-labelledby="headingTwo"
+                  data-parent="#accordion"
                 >
-                  05 Task Created
-                </label>
-              </h5>
-            </div>
-            <div
-              id="collapseTwo"
-              className="collapse"
-              aria-labelledby="headingTwo"
-              data-parent="#accordion"
-            >
-              <div className="card-body systemtaskreact">
-                <ReactTable
-                  data={datatask}
-                  columns={columnstask}
-                  // resizable={false}
-                  defaultPageSize={5}
-                  showPagination={false}
-                />
+                  <div className="card-body systemtaskreact">
+                    <ReactTable
+                      data={datatask}
+                      columns={columnstask}
+                      // resizable={false}
+                      defaultPageSize={5}
+                      showPagination={false}
+                    />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        </div>
         </div>
       </Fragment>
     );
