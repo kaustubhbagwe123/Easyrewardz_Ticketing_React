83c7c2dbb7f3616b6051a33000619769c966ae5a
Shlok Barot <shlok.barot@brainvire.com>
2020-01-22 10:41:13 +0530

    bug solve

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -3,6 +3,8 @@ let config = {
     //apiUrl: 'http://easyrewardz.brainvire.net:44357/api'
     // apiUrl: 'http://easyrewardz.demo.brainvire.net'
     //apiUrl: 'http://10.50.250.18:9512/api'
+    //apiUrl: 'https://ertktapi.dcdev.brainvire.net/Api'
   };
+  
   
   export default config;
