1c145263d60f07c9e95349feeaba442b98c53524
amit.kamble <amit.kamble@BVIMUMLAP-65>
2020-02-10 15:40:38 +0530

    added kb

--- src/routes/KnowledgeBase.js
+++ src/routes/KnowledgeBase.js
@@ -568,13 +568,15 @@ class KnowledgeBase extends Component {
       this.state.selectedSubject.length > 0
     ) {
       let self = this;
+      var ck=this.state.ckeditorAdd.replace("<p>","");
+      var ckeditor=ck.replace("</p>","");
       var json = {
         KBCODE: "",
         CategoryID: this.state.selectedCategory,
         SubCategoryID: this.state.selectedSubCategory,
 
         Subject: this.state.selectedSubject,
-        Description: this.state.ckeditorAdd,
+        Description: ckeditor,
         IsActive: 1,
         IssueTypeID: this.state.selectedIssueType
       };
