8a3c2e189d23408201616530241e62d5fd7f5c18
amit.kamble <amit.kamble@BVIMUMLAP-65>
2020-02-26 19:56:18 +0530

    added user kb

--- src/routes/Settings/Ticketing/Users.js
+++ src/routes/Settings/Ticketing/Users.js
@@ -1200,12 +1200,12 @@ if(datar==="add"){
     }).then(function (res) {
       debugger;
       let Msg = res.data.message;
-      if (Msg === "Success") {
-        NotificationManager.success("Record Deleted successfully.");
+      if (Msg === "Record In use") {
+        NotificationManager.error("Record in use.");
+        
+      }else if(Msg==="Record deleted Successfully") {
+        NotificationManager.success("Record deleted Successfully.");
         self.handleUserList();
-      }
-      else {
-        NotificationManager.error("Record Not Deleted.");
       }
     });
   }
