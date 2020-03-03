e690f385657f4cd72f8e9648c40ba411c05e0f52
Chetan Kokal <chetan.kokal@brainvire.com>
2020-03-03 13:56:33 +0530

    checkbox selection count sla bug solved

--- src/routes/Settings/Ticketing/CreateSLA.js
+++ src/routes/Settings/Ticketing/CreateSLA.js
@@ -172,7 +172,7 @@ class CreateSLA extends Component {
       // var separator = ",";
       // var values = indiSla.split(separator);
       for (var i = 0; i < values.length; i++) {
-        if (values[i] === issueId) {
+        if (values[i] == issueId) {
           values.splice(i, 1);
           indiSla = values.join(separator);
         }
