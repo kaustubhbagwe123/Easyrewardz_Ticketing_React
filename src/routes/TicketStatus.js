de494069f640837dd9f211819e1e542021533a71
Brainvire <techsupport@brainvire.com>
2020-03-04 17:18:19 +0530

    Default Report download popup changes

--- src/routes/TicketStatus.js
+++ src/routes/TicketStatus.js
@@ -7,7 +7,10 @@ function TicketStatus() {
     { ticketStatusID: 102, ticketStatusName: "Open" },
     { ticketStatusID: 103, ticketStatusName: "Resolved" },
     { ticketStatusID: 104, ticketStatusName: "Closed" },
-    { ticketStatusID: 105, ticketStatusName: "ReOpened" }
+    { ticketStatusID: 105, ticketStatusName: "ReOpened" },
+    { ticketStatusID: 1001, ticketStatusName: "Escalated" },
+    { ticketStatusID: 1004, ticketStatusName: "Re-assigned" },
+
   ];
 }
 
