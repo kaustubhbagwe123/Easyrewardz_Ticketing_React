4008efc6d740d94c8f6df363eb6f2a86f35e0b5b
Chetan Kokal <chetan.kokal@brainvire.com>
2020-03-20 17:25:38 +0530

    solved Invalid Date bug in myTicket page

--- src/routes/MyTicket.js
+++ src/routes/MyTicket.js
@@ -3870,17 +3870,18 @@ class MyTicket extends Component {
                                             <div className="col-sm-12 p-0">
                                               <DatePicker
                                                 selected={
+                                                  row.original.storeVisitDate !== null ?
                                                   new Date(
                                                     row.original.storeVisitDate
-                                                  )
+                                                  ) : new Date()
                                                 }
-                                                // placeholderText="MM/DD/YYYY"
-                                                placeholderText={
-                                                  row.original
-                                                    .storeVisitDate === null
-                                                    ? "MM/DD/YYYY"
-                                                    : null
-                                                }
+                                                placeholderText="MM/DD/YYYY"
+                                                // placeholderText={
+                                                //   row.original
+                                                //     .storeVisitDate === null
+                                                //     ? "MM/DD/YYYY"
+                                                //     : null
+                                                // }
                                                 showMonthDropdown
                                                 showYearDropdown
                                                 dateFormat="MM/DD/YYYY"
@@ -3888,9 +3889,9 @@ class MyTicket extends Component {
                                                   "visitDate" +
                                                   row.original.storeID
                                                 }
-                                                value={moment(
+                                                value={row.original.storeVisitDate !== null ? moment(
                                                   row.original.storeVisitDate
-                                                ).format("MM/DD/YYYY")}
+                                                ).format("MM/DD/YYYY") : ''}
                                                 // name="visitDate"
                                                 onChange={this.handleByvisitDate.bind(
                                                   this,
