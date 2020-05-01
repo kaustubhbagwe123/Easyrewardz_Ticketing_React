2d537b8a87a939aaa8ffe6c561f741ed6c1fccb0
Deepaksavani <deepak.savani@brainvire.com>
2020-05-01 19:51:02 +0530

    wokring shcedule tab.

--- src/routes/StoreManager/store-header.js
+++ src/routes/StoreManager/store-header.js
@@ -32,11 +32,14 @@ import { transferData } from "./../../helpers/transferData";
 import "./../../assets/css/store-chat.css";
 import CKEditor from "ckeditor4-react";
 import SearchBlueImg from "./../../assets/Images/search-blue.png";
-import Bata from "./../../assets/Images/Bata2.jpeg";
+import Bata from "./../../assets/Images/Bata2.jpg";
 import DownArrow from "./../../assets/Images/down.png";
 import RightBlue from "./../../assets/Images/rightblue.png";
 import UpBlue from "./../../assets/Images/new-Up.png";
 import DownBlue from "./../../assets/Images/new-Down.png";
+import CircleRight from "./../../assets/Images/circle-right.png";
+import ReactHtmlParser from "react-html-parser";
+import { Tooltip } from 'antd';
 
 class Header extends Component {
   constructor(props) {
@@ -162,6 +165,9 @@ class Header extends Component {
       isDownbtn: true,
       customerName: "",
       messageData: [],
+      message: "",
+      chatMessageCount: 0,
+      activeTab: 1,
     };
     this.handleNotificationModalClose = this.handleNotificationModalClose.bind(
       this
@@ -212,6 +218,7 @@ class Header extends Component {
         1
       );
       this.handleGetNotigfication();
+      this.handleGetChatNotificationCount();
     }
   }
 
@@ -264,7 +271,8 @@ class Header extends Component {
       logoBlue: CampaignLogoBlue,
       imgAlt: "campaign icon",
       imgClass: "campaign-icon",
-      activeClass: page === "Appointment" ? "active single-menu" : "single-menu",
+      activeClass:
+        page === "Appointment" ? "active single-menu" : "single-menu",
     };
     if (data !== null) {
       for (var i = 0; i < data.length; i++) {
@@ -288,13 +296,12 @@ class Header extends Component {
           data[i].modulestatus === true
         ) {
           accessdata.push(campaign);
-        }else if (
+        } else if (
           data[i].moduleName === "Appointment" &&
           data[i].modulestatus === true
         ) {
           accessdata.push(appointment);
-        }
-        else if (
+        } else if (
           data[i].moduleName === "Settings" &&
           data[i].modulestatus === true
         ) {
@@ -557,6 +564,11 @@ class Header extends Component {
         var ongoingChatsData = response.data.responseData;
         if (message === "Success" && ongoingChatsData) {
           self.setState({ ongoingChatsData });
+          // setInterval(() => {
+          // if (self.state.chatModal) {
+          //   self.handleGetOngoingChat();
+          // }
+          // }, 40000);
         } else {
           self.setState({ ongoingChatsData });
         }
@@ -579,6 +591,11 @@ class Header extends Component {
         var newChatsData = response.data.responseData;
         if (message === "Success" && newChatsData) {
           self.setState({ newChatsData });
+          // setInterval(() => {
+          // if (self.state.chatModal) {
+          //   self.handleGetNewChat();
+          // }
+          // }, 40000);
         } else {
           self.setState({ newChatsData });
         }
@@ -653,7 +670,14 @@ class Header extends Component {
         var message = response.data.message;
         var messageData = response.data.responseData;
         if (message === "Success" && messageData) {
-          self.setState({ messageData });
+          self.setState({
+            messageData,
+          });
+          // setInterval(() => {
+          // if (self.state.chatModal) {
+          self.handleGetChatMessagesList(id);
+          //   }
+          // }, 20000);
         } else {
           self.setState({ messageData });
         }
@@ -662,31 +686,105 @@ class Header extends Component {
         console.log(response, "---handleGetChatMessagesList");
       });
   }
+  ////handle save chat messgae
   handleSaveChatMessages() {
     let self = this;
-    // var inputParam={}
-
+    var inputParam = {};
+    inputParam.chatID = this.state.chatId;
+    inputParam.message = this.state.message;
+    inputParam.attachment = null;
+    inputParam.byCustomer = false;
+    inputParam.chatStatus = 0;
+    inputParam.storeManagerId = 0;
     axios({
       method: "post",
       url: config.apiUrl + "/CustomerChat/saveChatMessages",
       headers: authHeader(),
-      data: {
-        // chatID: id,
-      },
+      data: inputParam,
     })
       .then(function(response) {
         var message = response.data.message;
-        var messageData = response.data.responseData;
-        if (message === "Success" && messageData) {
-          self.setState({ messageData });
+        var responseData = response.data.responseData;
+        if (message === "Success" && responseData) {
+          self.handleGetChatMessagesList(self.state.chatId);
         } else {
-          self.setState({ messageData });
         }
       })
       .catch((response) => {
         console.log(response, "---handleGetChatMessagesList");
       });
   }
+  ////handle get chat notification count
+  handleGetChatNotificationCount() {
+    let self = this;
+    axios({
+      method: "post",
+      url: config.apiUrl + "/CustomerChat/GetChatNotificationCount",
+      headers: authHeader(),
+    })
+      .then(function(response) {
+        var message = response.data.message;
+        var chatMessageCount = response.data.responseData;
+        self.setState({ chatMessageCount });
+        // setInterval(() => {
+        // self.handleGetChatNotificationCount();
+        // }, 30000);
+      })
+      .catch((response) => {
+        console.log(response, "---handleGetChatNotificationCount");
+      });
+  }
+
+  ////handle get chat notification count
+  handleSearchChatItemDetails() {
+    let self = this;
+    axios({
+      method: "post",
+      url: config.apiUrl + "/CustomerChat/searchChatItemDetails",
+      headers: authHeader(),
+      params: {
+        SearchText: "",
+      },
+    })
+      .then(function(response) {
+        var message = response.data.message;
+        var responseData = response.data.responseData;
+
+        if (message == "Success" && responseData) {
+        }
+      })
+      .catch((response) => {
+        console.log(response, "---handleSearchChatItemDetails");
+      });
+  }
+  ////handle get chat notification count
+  handleGetTimeSlot() {
+    let self = this;
+
+    axios({
+      method: "post",
+      url: config.apiUrl + "/CustomerChat/GetTimeSlot",
+      headers: authHeader(),
+      params: {
+        storeID: 1,
+      },
+    })
+      .then(function(response) {
+        var message = response.data.message;
+        var timeSlotData = response.data.responseData;
+
+        if (message == "Success" && timeSlotData) {
+          self.setState({ timeSlotData });
+        } else {
+          self.setState({ timeSlotData });
+        }
+      })
+      .catch((response) => {
+        console.log(response, "---handleGetTimeSlot");
+      });
+  }
+
+  ////handlecselect card in card tab
   handleSelectCard(id) {
     if (
       this.state.searchCardData.filter((x) => x.id === id)[0]["isSelect"] ==
@@ -704,9 +802,30 @@ class Header extends Component {
       searchCardData: this.state.searchCardData,
     });
   }
+  ////handle button down click
   handleDownButtonClick() {
     this.setState({ isDownbtn: !this.state.isDownbtn });
   }
+  ////handle on change ck editor
+  handleOnChangeCKEditor = (evt) => {
+    var message = evt.editor.getData();
+    this.setState({
+      message,
+    });
+  };
+  ////handle on going chat click
+  handleOngoingChatClick = (id, name, count) => {
+    this.setState({ customerName: name });
+    if (this.state.chatId === id) {
+      this.handleGetChatMessagesList(id);
+    } else {
+      if (count === 0) {
+        this.handleGetChatMessagesList(id);
+      } else {
+        this.handleMakeAsReadOnGoingChat(id, name);
+      }
+    }
+  };
   render() {
     return (
       <React.Fragment>
@@ -805,7 +924,9 @@ class Header extends Component {
                   className="chatImg"
                   style={{ display: "none" }}
                 />
-                <span className="message-icon-cnt">3</span>
+                <span className="message-icon-cnt">
+                  {this.state.chatMessageCount}
+                </span>
               </div>
             </a>
             {/* --notification-- */}
@@ -1114,10 +1235,11 @@ class Header extends Component {
                                 ? "chat-info active"
                                 : "chat-info"
                             }
-                            onClick={this.handleMakeAsReadOnGoingChat.bind(
+                            onClick={this.handleOngoingChatClick.bind(
                               this,
                               chat.chatID,
-                              chat.cumtomerName
+                              chat.cumtomerName,
+                              chat.messageCount
                             )}
                           >
                             <div className="d-flex align-items-center overflow-hidden">
@@ -1147,7 +1269,7 @@ class Header extends Component {
                   <div className="chat-cntr">
                     <p className="chats-heading">
                       New Chats (
-                      {this.state.newChatsData.length < 10
+                      {this.state.newChatsData.length < 9
                         ? "0" + this.state.newChatsData.length
                         : this.state.newChatsData.length}
                       )
@@ -1207,10 +1329,10 @@ class Header extends Component {
                       />
                     </div>
                   </div> */}
-                  <ul class="nav nav-tabs" role="tablist">
-                    <li class="nav-item">
+                  <ul className="nav nav-tabs" role="tablist">
+                    <li className="nav-item">
                       <a
-                        class="nav-link active"
+                        className="nav-link active"
                         id="ongoing-chat-tab"
                         data-toggle="tab"
                         href="#ongoing-chat"
@@ -1221,9 +1343,9 @@ class Header extends Component {
                         Ongoing Chats
                       </a>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                       <a
-                        class="nav-link"
+                        className="nav-link"
                         id="new-chat-tab"
                         data-toggle="tab"
                         href="#new-chat"
@@ -1236,9 +1358,9 @@ class Header extends Component {
                     </li>
                   </ul>
                 </div>
-                <div class="tab-content">
+                <div className="tab-content">
                   <div
-                    class="tab-pane fade show active"
+                    className="tab-pane fade show active"
                     id="ongoing-chat"
                     role="tabpanel"
                     aria-labelledby="ongoing-chat-tab"
@@ -1255,10 +1377,11 @@ class Header extends Component {
                                     ? "chat-detail-cntr active"
                                     : "chat-detail-cntr"
                                 }
-                                onClick={this.handleMakeAsReadOnGoingChat.bind(
+                                onClick={this.handleOngoingChatClick.bind(
                                   this,
                                   chat.chatID,
-                                  chat.cumtomerName
+                                  chat.cumtomerName,
+                                  chat.messageCount
                                 )}
                               >
                                 <div className="chat-face-cntr">
@@ -1278,7 +1401,7 @@ class Header extends Component {
                     </div>
                   </div>
                   <div
-                    class="tab-pane fade"
+                    className="tab-pane fade"
                     id="new-chat"
                     role="tabpanel"
                     aria-labelledby="new-chat-tab"
@@ -1347,9 +1470,10 @@ class Header extends Component {
                                     </div>
                                     <div className="chat-trail-chat-cntr">
                                       <p className="chat-trail-chat">
-                                        {item.message}
+                                        {ReactHtmlParser(item.message)}
                                       </p>
                                       <span className="chat-trail-time">
+                                        {item.chatDate + " "}
                                         {item.chatTime}
                                       </span>
                                     </div>
@@ -1418,6 +1542,7 @@ class Header extends Component {
                             role="tab"
                             aria-controls="schedule-visit-tab"
                             aria-selected="false"
+                            onClick={this.handleGetTimeSlot.bind(this)}
                           >
                             SCHEDULE VISIT
                           </a>
@@ -1445,6 +1570,8 @@ class Header extends Component {
                         >
                           <div className="message-div">
                             <CKEditor
+                              data={this.state.message}
+                              onChange={this.handleOnChangeCKEditor}
                               config={{
                                 toolbar: [
                                   {
@@ -1471,7 +1598,11 @@ class Header extends Component {
                                 ],
                               }}
                             />
-                            <div className="mobile-ck-send">
+
+                            <div
+                              className="mobile-ck-send"
+                              onClick={this.handleSaveChatMessages.bind(this)}
+                            >
                               <img src={Assign} alt="send img" />
                             </div>
                           </div>
@@ -1607,7 +1738,59 @@ class Header extends Component {
                           id="schedule-visit-tab"
                           role="tabpanel"
                           aria-labelledby="schedule-visit-tab"
-                        ></div>
+                        >
+                          <div
+                            className="row"
+                            style={{ marginLeft: "5px", marginTop: "10px" }}
+                          >
+                            <div className="col-md-8">
+                              <div>
+                                <label className="s-lable">
+                                  Today:11 May 2020
+                                </label>
+                                <div style={{ display: "inline-block" }}>
+                                  <button className="s-red-btn">
+                                    11AM-12PM
+                                    <img
+                                      className="s-img-select"
+                                      src={CircleRight}
+                                      alt="circle-right"
+                                    />
+                                  </button>
+                                  <button className="s-green-btn">
+                                    11AM-12PM
+                                  </button>
+                                  <button className="s-yellw-btn">
+                                    11AM-12PM
+                                  </button>
+                                  <button className="s-green-btn ">
+                                    11AM-12PM
+                                  </button>
+                                </div>
+                                <div className="selectdot-blue"></div>
+                              </div>
+                            </div>
+                            <div className="col-md-4">
+                              <div className="row">
+                                <div>
+                                  <label>Select Slot</label>
+                                  <button className="s-green-active">
+                                    2PM-3PM
+                                    <img
+                                      className="s-img-select"
+                                      src={CircleRight}
+                                      alt="circle-right"
+                                    />
+                                  </button>
+                                </div>
+                                <div>
+                                  <label>No of People</label>
+                                  <input type="text" />
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                         {/* --------Generate Payment Link Tab----- */}
                         <div
                           className="tab-pane fade"
