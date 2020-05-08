02442173296c293a10ce31c111500f0541f9c0f9
Shlok Barot <shlok.barot@brainvire.com>
2020-05-08 14:54:36 +0530

    remove uncommented code

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,60 +1,6 @@
 let config = {
   apiUrl: "https://localhost:44357/api",
-  // hierarchyTemplate: [["Designation", "ReportTo", "Status"]],
-  // priorityTemplate: [["PriorityName", "Status"]],
-  // userTemplate: [
-  //   [
-  //     "UserName",
-  //     "FirstName",
-  //     "LastName",
-  //     "Mobile Number",
-  //     "User Email ID",
-  //     "User Designation",
-  //     "Reportee Designation",
-  //     "Reports To",
-  //     "CRM Role",
-  //     "Copy Escalation",
-  //     "Assign Escalation",
-  //     "Escalation Agent Name",
-  //     "Status",
-  //   ],
-  // ],
-  // alertTemplate: [["AlertType", "Communication Mode", "Status"]],
-  // categoryTemplate: [
-  //   ["BrandName", "Category", "SubCategory", "IssueType", "Status"],
-  // ],
-  // // slaTemplate: [
-  // //   [
-  // //     "IssueType",
-  // //     "Priority",
-  // //     "SLABreachPercent",
-  // //     "RespondValue",
-  // //     "RespondDuration",
-  // //     "ResolutionValue",
-  // //     "ResolutionDuration",
-  // //     "Status",
-  // //   ],
-  // // ],
-  // crmRoleTemplate: [
-  //   ["RoleName", "Status", "EnabledModules", "DisabledModules"],
-  // ],
-  // storeTemplate: [
-  //   [
-  //     "Brand",
-  //     "City",
-  //     "State",
-  //     "PinCode",
-  //     "StoreName",
-  //     "Address",
-  //     "StoreCode",
-  //     "Region",
-  //     "Zone",
-  //     "StoreType",
-  //     "StoreEmailID",
-  //     "StorePhoneNo",
-  //     "Status",
-  //   ],
-  // ],
+ 
   //// -----------------------[Ticketing bulkupload code]--------------------------------------------
   hierarchyTemplate: [
     ["Designation", "ReportTo", "Status"],
@@ -315,7 +261,7 @@ let config = {
   //apiUrl: 'http://easyrewardz.brainvire.net:44357/api'
   // apiUrl: 'http://easyrewardertz.demo.brainvire.net'
   //apiUrl: "https://ertktapi.dcdev.brainvire.net/Api",
-  // apiUrl: "http://ertktapistore.dcdev.brainvire.net/Api", ///----Store API--------
+   //apiUrl: "http://ertktapistore.dcdev.brainvire.net/Api", ///----Store API--------
   //apiUrl: 'https://ertktapistable.dcdev.brainvire.net/Api'
   //apiUrl: 'http://stage-bellapi.ercx.co/Api'    /// ----- Client API -------
   // apiUrl: 'https://ertktapistable.dcdev.brainvire.net/Api'
