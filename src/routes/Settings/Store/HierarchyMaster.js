4048b9e358e242766188adcdf5cc77072d9d4e7e
Shlok Barot <shlok.barot@brainvire.com>
2020-05-11 19:20:58 +0530

    workin on cliam category master page in multi selecr brand- working

--- src/routes/Settings/Store/HierarchyMaster.js
+++ src/routes/Settings/Store/HierarchyMaster.js
@@ -1669,29 +1669,7 @@ class HierarchyMaster extends Component {
                 </div>
               </div>
               <div className="col-md-4">
-                {/* <div className="right-sect-div">
-                  <h3>Create Hierarchy</h3>
-                  <div className="div-cntr">
-                    <label>Designation Name</label>
-                    <input type="text" defaultValue="Store Manager" maxLength={25} />
-                  </div>
-                  <div className="div-cntr">
-                    <label>Report To</label>
-                    <select>
-                      <option>Select</option>
-                      <option>Root</option>
-                      <option>Root</option>
-                    </select>
-                  </div>
-                  <div className="div-cntr">
-                    <label>Status</label>
-                    <select>
-                      <option>Active</option>
-                      <option>Inactive</option>
-                    </select>
-                  </div>
-                  <button className="butn">ADD</button>
-                </div> */}
+              
                 <div className="createHierarchyMask">
                   <div className="createSpace">
                     <label className="create-department">
@@ -1724,8 +1702,8 @@ class HierarchyMaster extends Component {
                           value={this.state.selectReportTo}
                           onChange={this.handleOnReportToChange}
                         >
-                          <option value="0">select</option>
-                          <option value={1}>Root</option>
+                          <option value="">select</option>
+                          <option value={-1}>Root</option>
                           {this.state.reportToData !== null &&
                             this.state.reportToData.map((item, i) => (
                               <option key={i + 1} value={item.designationID}>
@@ -1733,7 +1711,7 @@ class HierarchyMaster extends Component {
                               </option>
                             ))}
                         </select>
-                        {parseInt(this.state.selectReportTo) === 0 && (
+                        {parseInt(this.state.selectReportTo) === "" && (
                           <p style={{ color: "red", marginBottom: "0px" }}>
                             {this.state.reportToCompulsion}
                           </p>
