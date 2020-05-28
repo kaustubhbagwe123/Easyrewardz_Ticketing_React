7d7bbe67835440cc1b81672581eebd9947ade146
Rashmi S_BVI <rashmi.salaria@brainvire.com>
2020-05-28 17:19:49 +0530

    create appointment Implementation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { MyProvider, MyContext } from './context'
 import "react-app-polyfill/ie9";
 import "react-app-polyfill/ie11";
 
@@ -46,30 +47,38 @@ import StoreChangePassword from "./Component/Store/ChangePassword";
 import LanguageSelection from "./Component/Store/LanguageSelection";
 
 ReactDOM.render(
-  <Router>
-    <Route exact path="/ModuleSelect" component={ModuleSelect} />
-    <Route path="/admin" component={App} />
-    <Route path="/store" component={StoreApp} />
-    <Route path="/qa" component={QAApp} />
-    <Route exact path="/ProgramCodeSignIn" component={ProgramCodeSignIn} />
-    <Route exact path="/SignIn" component={SignIn} />
-    <Route exact path="/Forgotpassword" component={ForgotPassword} />
-    <Route exact path="/ChangePassword" component={ChangePassword} />
-    <Route exact path="/storeChangePassword" component={StoreChangePassword} />
-    <Route exact path="/languageSelection" component={LanguageSelection} />
-    <Route exact path="/" component={StoreProgramCode} />
-    <Route exact path="/storeSignIn" component={StoreSignIn} />
-    <Route exact path="/storeForgotpassword" component={StoreForgotPassword} />
-    <Route
-      exact
-      path="/storeUserForgotPassword"
-      component={StoreUserForgotPassword}
-    />
-    <Route exact path="/UserForgotPassword" component={UserForgotPassword} />
-    <Route exact path="/PieChart" component={PieChart} />
-    <Route path="/artBoard" component={ArtBoardApp} />
-    <Route path="/tenantApp" component={TenantApp} />
-  </Router>,
+  <MyProvider>
+    <Router>
+      <MyContext.Consumer>
+        {(context) => (
+          <React.Fragment>
+            <Route exact path="/ModuleSelect" component={ModuleSelect} />
+            <Route path="/admin" component={App} />
+            <Route path="/store" component={StoreApp} />
+            <Route path="/qa" component={QAApp} />
+            <Route exact path="/ProgramCodeSignIn" component={ProgramCodeSignIn} />
+            <Route exact path="/SignIn" component={SignIn} />
+            <Route exact path="/Forgotpassword" component={ForgotPassword} />
+            <Route exact path="/ChangePassword" component={ChangePassword} />
+            <Route exact path="/storeChangePassword" component={StoreChangePassword} />
+            <Route exact path="/languageSelection" component={LanguageSelection} />
+            <Route exact path="/" component={StoreProgramCode} />
+            <Route exact path="/storeSignIn" component={StoreSignIn} />
+            <Route exact path="/storeForgotpassword" component={StoreForgotPassword} />
+            <Route
+              exact
+              path="/storeUserForgotPassword"
+              component={StoreUserForgotPassword}
+            />
+            <Route exact path="/UserForgotPassword" component={UserForgotPassword} />
+            <Route exact path="/PieChart" component={PieChart} />
+            <Route path="/artBoard" component={ArtBoardApp} />
+            <Route path="/tenantApp" component={TenantApp} />
+          </React.Fragment>
+        )}
+      </MyContext.Consumer>
+    </Router>
+  </MyProvider>,
   document.getElementById("root")
 );
 
