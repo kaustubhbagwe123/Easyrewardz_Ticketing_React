340ab661e5d5cd58feba8b22304a301144a573ea
Chetan Kokal <chetan.kokal@brainvire.com>
2020-05-29 12:24:38 +0530

    normal commit

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -244,7 +244,7 @@ let config = {
     ["Issue1", "Low", "30", "2", "Day", "Active"],
   ],
   //apiUrl: 'https://api-shopsterqa.ercx.co/Api',  /// -------HomeShopClient API----------
-   apiUrl: "https://ertktapihomeshop.dcdev.brainvire.net/Api", /// -------HomeShop API---------
+  apiUrl: "https://ertktapihomeshop.dcdev.brainvire.net/Api", /// -------HomeShop API---------
   //apiUrl: "https://ertktapi.dcdev.brainvire.net/Api",
   //apiUrl: "http://ertktapistore.dcdev.brainvire.net/Api", ///----Store API--------
   //apiUrl: 'https://ertktapistable.dcdev.brainvire.net/Api'
