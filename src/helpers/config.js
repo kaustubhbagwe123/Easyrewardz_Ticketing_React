a47b8b77b7a562e95899b086d1205af56f48c29f
Prince Mishra <prince.mishra@brainvire.com>
2020-06-06 18:36:31 +0530

    work on multilingual1.0

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,5 +1,5 @@
 let config = {
-  //apiUrl: "https://localhost:44357/api",
+  apiUrl: "https://localhost:44357/api",
   isHomeShope:true, /// make false when give Homeshop build for client
 
   //// -----------------------[Ticketing bulkupload code]--------------------------------------------
@@ -246,7 +246,7 @@ let config = {
   ],
   //apiUrl: 'https://api-shopsterqa.ercx.co/Api',  /// -------HomeShopClient API----------
   //  apiUrl: "https://ertktapihomeshop.dcdev.brainvire.net/Api", /// -------HomeShop API---------
-  apiUrl: "https://multitenancyshopsterapi.dcdev.brainvire.net/Api", /// -------HomeShopMultiTenant API---------
+  //apiUrl: "https://multitenancyshopsterapi.dcdev.brainvire.net/Api", /// -------HomeShopMultiTenant API---------
   //apiUrl: "http://ertktapistore.dcdev.brainvire.net/Api", ///----Store API--------
   //apiUrl: 'https://ertktapistable.dcdev.brainvire.net/Api'
   //apiUrl: 'http://stage-bellapi.ercx.co/Api'    /// ----- Client API -------
