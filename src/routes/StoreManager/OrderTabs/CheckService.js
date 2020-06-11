fce18f14a572a4705a8263cc8eb11c05d7a50e40
Shlok Barot <shlok.barot@brainvire.com>
2020-06-11 16:41:09 +0530

    git bug solved

--- src/routes/StoreManager/OrderTabs/CheckService.js
+++ src/routes/StoreManager/OrderTabs/CheckService.js
@@ -1,21 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class CheckService extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             
-        }
-    }
-    
-    render() {
-        return (
-            <>
-                
-            </>
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <>
+        <div className="check-svr">
+          <div className="row m-b-10">
+            <div className="col-md-5">
+              <label className="">Store Pin Code</label>
+            </div>
+            <div className="col-md-6">
+              <input
+                type="text"
+                className="txt-1"
+                placeholder="Store PIN Code"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-5">
+              <label className="">Enter Pin Code</label>
+            </div>
+            <div className="col-md-6">
+              <input type="text" className="txt-1" placeholder="PIN Code" />
+            </div>
+          </div>
+          <div className="row">
+            <button class="check-svcBtn">Submit</button>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
-export default CheckService
+export default CheckService;
