a34ba8cd38b522d241f1a1dff96edaa802df4dd4
Shlok Barot <shlok.barot@brainvire.com>
2020-06-12 12:31:34 +0530

    Binding and update check serive on order page

--- src/routes/StoreManager/OrderTabs/CheckService.js
+++ src/routes/StoreManager/OrderTabs/CheckService.js
@@ -1,12 +1,78 @@
 import React, { Component } from "react";
-
+import axios from "axios";
+import { authHeader } from "../../../helpers/authHeader";
+import config from "../../../helpers/config";
+import { NotificationManager } from "react-notifications";
 class CheckService extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      storePinCode: "",
+      pin_code: "",
+    };
   }
 
+  componentDidMount() {
+    this.handleGetCheckServiceData();
+  }
+
+  /// handle Get Check service data
+  handleGetCheckServiceData() {
+    let self = this;
+    axios({
+      method: "post",
+      url: config.apiUrl + "/HSOrder/GetStorePinCodeByUserID",
+      headers: authHeader(),
+    })
+      .then(function(res) {
+        debugger;
+        let status = res.data.message;
+        let data = res.data.responseData;
+        if (status === "Success") {
+          self.setState({
+            storePinCode: data,
+          });
+        } else {
+          self.setState({
+            storePinCode: "",
+          });
+        }
+      })
+      .catch((data) => {
+        console.log(data);
+      });
+  }
+  /// handle Submit Check service data
+  handleGetCheckServiceData() {
+    debugger;
+    axios({
+      method: "post",
+      url: config.apiUrl + "/HSOrder/CheckCourierAvailibilty",
+      headers: authHeader(),
+      data: {
+        Pickup_postcode: parseInt(this.state.storePinCode),
+        Delivery_postcode: parseInt(this.state.pin_code),
+      },
+    })
+      .then(function(res) {
+        let status = res.data.message;
+        if (status === "Success") {
+          NotificationManager.success("Available.");
+        } else {
+          NotificationManager.success("Not Available.");
+        }
+      })
+      .catch((data) => {
+        console.log(data);
+      });
+  }
+  /// handle Text onchage
+  handleTextOnchange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
   render() {
     return (
       <>
@@ -20,6 +86,7 @@ class CheckService extends Component {
                 type="text"
                 className="txt-1"
                 placeholder="Store PIN Code"
+                value={this.state.storePinCode}
               />
             </div>
           </div>
@@ -28,11 +95,23 @@ class CheckService extends Component {
               <label className="naman">Enter Pin Code</label>
             </div>
             <div className="col-md-6">
-              <input type="text" className="txt-1" placeholder="PIN Code" />
+              <input
+                type="text"
+                className="txt-1"
+                placeholder="PIN Code"
+                name="pin_code"
+                value={this.state.pin_code}
+                onChange={this.handleTextOnchange}
+              />
             </div>
           </div>
 
-          <button class="check-svcBtn">Submit</button>
+          <button
+            className="check-svcBtn"
+            onClick={this.handleGetCheckServiceData.bind(this)}
+          >
+            Submit
+          </button>
         </div>
       </>
     );
