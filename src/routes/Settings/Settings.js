9ae1383e956f8d319f27498936da5fe01a575a36
Shlok Barot <shlok.barot@brainvire.com>
2020-06-18 15:20:29 +0530

    phase 1 setting page bug solved

--- src/routes/Settings/Settings.js
+++ src/routes/Settings/Settings.js
@@ -36,7 +36,8 @@ class Settings extends Component {
   }
 
   componentDidMount() {
-    var isReport = document.getElementById("isReport").value;
+    debugger;
+    var isReport = document.getElementById("isReport").value || "none";
 
     if (isReport === "block") {
       this.setState({ showReport: true });
@@ -793,12 +794,16 @@ class Settings extends Component {
                             <img src={modules} alt="modules" />
                           </div>
                           <div className="setting-desc">
-                            <strong>{TranslationContext!==undefined?TranslationContext.strong.ordersetting:"Order Settings"}</strong>
+                            <strong>
+                              {TranslationContext !== undefined
+                                ? TranslationContext.strong.ordersetting
+                                : "Order Settings"}
+                            </strong>
                             <p>
-                            {TranslationContext !== undefined
-                              ? TranslationContext.p
-                                  .asysteminwhichmembersofanorganizationorsocietyarerankedaccordingtorelativestatusorauthority
-                              : "A system in which members of an organization or society are ranked according to relative status or authority."}
+                              {TranslationContext !== undefined
+                                ? TranslationContext.p
+                                    .asysteminwhichmembersofanorganizationorsocietyarerankedaccordingtorelativestatusorauthority
+                                : "A system in which members of an organization or society are ranked according to relative status or authority."}
                             </p>
                           </div>
                         </Link>
