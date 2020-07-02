0dcc3fc91fd2b95a9e3f5b5c95e3f919e0385d92
Chetan Kokal <chetan.kokal@brainvire.com>
2020-07-02 15:55:34 +0530

    solved few bugs in Ticketing phase

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ import LanguageSelection from "./Component/Store/LanguageSelection";
 ReactDOM.render(
   <Router>
     {/* if required the only on uncomment this code and your system do not push this line*/}
-    <Route exact path="/" component={ModuleSelect} />
+    {/* <Route exact path="/" component={ModuleSelect} /> */}
     <Route path="/admin" component={App} />
     <Route path="/store" component={StoreApp} />
     <Route path="/qa" component={QAApp} />
@@ -58,7 +58,7 @@ ReactDOM.render(
     <Route exact path="/ChangePassword" component={ChangePassword} />
     <Route exact path="/storeChangePassword" component={StoreChangePassword} />
     <Route exact path="/languageSelection" component={LanguageSelection} />
-    <Route exact path="/storeProgramCode" component={StoreProgramCode} />
+    <Route exact path="/" component={StoreProgramCode} />
     <Route exact path="/storeSignIn" component={StoreSignIn} />
     <Route exact path="/storeForgotpassword" component={StoreForgotPassword} />
     <Route
