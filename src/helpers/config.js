df5b4274c688c4883332bfaaa3267a970961ae04
Shlok Barot <shlok.barot@brainvire.com>
2020-07-13 11:36:48 +0530

    first take pull

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,7 +1,7 @@
 let config = {
   apiUrl: "https://localhost:44357/api",
   isHomeShope: true, /// make false when give Homeshop build for client
-  isShowTaskTab: true, /// make false when give multiTenant build for client
+  isShowTaskTab: true, /// make false when give multiTenant store module build for client
 
   //// -----------------------[Ticketing bulkupload code]--------------------------------------------
   hierarchyTemplate: [
@@ -252,11 +252,12 @@ let config = {
   //apiUrl: "http://ertktapistore.dcdev.brainvire.net/Api", ///----Store API--------
   //apiUrl: 'https://ertktapistable.dcdev.brainvire.net/Api'
   //apiUrl: 'http://stage-bellapi.ercx.co/Api'    /// ----- Client API -------
+  //apiUrl: 'https://api-bell-tktqa.ercx.co/Api'    /// ----- Client API for store module-------
   // apiUrl: 'https://ertktapistable.dscdev.brainvire.net/Api'
   // socketUrl: "http://ndjs.shopster.live/api/sendreply", /// ------Client Socket--------
   // socketUrl: "wss://bvsocketserver.dcdev.brainvire.net",
-  socketUrl: "https://bvsocketservermts.dcdev.brainvire.net", /// -------Socket MultiTenant URL---------
-  //socketUrl: "https://api-bellchatsocketserverqa.ercx.co", /// -------Socket URL Client---------
+  //socketUrl: "https://bvsocketservermts.dcdev.brainvire.net", /// -------Socket MultiTenant URL---------
+  socketUrl: "https://api-bellchatsocketserverqa.ercx.co", /// -------Socket URL Client---------
   // socketUrl:"http://localhost:4000/"
   // soundURL: "https://localhost:44357/Uploadfiles/Chat/ChatBotSoundFiles/",
   soundURL: "https://multitenancyshopsterapiv2.dcdev.brainvire.net/Uploadfiles/Chat/ChatBotSoundFiles/",
