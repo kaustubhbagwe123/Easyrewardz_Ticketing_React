319183fa4db93af1aeeff8c93ed4e4f6441d09e1
Shlok Barot <shlok.barot@brainvire.com>
2020-07-16 11:58:50 +0530

    take pull

--- src/Component/Store/StoreSignIn.js
+++ src/Component/Store/StoreSignIn.js
@@ -44,8 +44,6 @@ class StoreSignIn extends Component {
   };
 
   componentDidMount() {
-    debugger;
-
     if (this.props.location.encProgramCode) {
       var finalEncProgramCode = this.props.location.encProgramCode;
 
@@ -71,11 +69,11 @@ class StoreSignIn extends Component {
 
       let X_Authorized_password = encryption(password, "enc");
 
-      // let X_Authorized_Domainname = encryption(window.location.origin, "enc");
-      let X_Authorized_Domainname = encryption(
-        "https://multitenancyshopsterv2.dcdev.brainvire.net",
-        "enc"
-      );
+      let X_Authorized_Domainname = encryption(window.location.origin, "enc");
+      // let X_Authorized_Domainname = encryption(
+      //   "https://multitenancyshopsterv2.dcdev.brainvire.net",
+      //   "enc"
+      // );
       // let X_Authorized_Domainname = encryption(
       //   "https://multitenancyshopster.dcdev.brainvire.net",
       //   "enc"
